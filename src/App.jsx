cf14e57207da32f1a472267d45a342352191103d
AshKemp <kmuraliashwin@gmail.com>
2024-08-10 18:10:41 +0530

    Use Outlet to have a common layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CommentsList from "./pages/comments";
 import ReceipeList from "./pages/receipes";
 import ReceipeDetails from "./pages/receipe-details";
 import NotFound from "./pages/not-found";
+import Layout from "./pages/components/layout";
 
 function App() {
   const navigate = useNavigate();
@@ -10,26 +11,29 @@ function App() {
     <>
       <h1>React routing and custom hooks</h1>
       <div>
-        <Link to={"/receipe-list"}>
+        <Link to={"/home/receipe-list"}>
           Alternative way of navigating to receipt list page
         </Link>
       </div>
       <button
-        onClick={() => navigate("/receipe-list")}
+        onClick={() => navigate("/home/receipe-list")}
         style={{ background: "black", color: "white" }}
       >
         Navigate to receipt list page
       </button>
       <button
-        onClick={() => navigate("/comments-list")}
+        onClick={() => navigate("/home/comments-list")}
         style={{ background: "black", color: "white" }}
       >
         Navigate to comments list page
       </button>
       <Routes>
-        <Route path="/receipe-list" element={<ReceipeList />} />
-        <Route path="/comments-list" element={<CommentsList />} />
-        <Route path="/receipe-list/:id" element={<ReceipeDetails />} />
+        <Route path="/home" element={<Layout />}>
+          <Route path="receipe-list" element={<ReceipeList />} />
+          <Route path="comments-list" element={<CommentsList />} />
+          <Route path="receipe-list/:id" element={<ReceipeDetails />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
---
+++ src/pages/components/header/index.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <div>HomePage</div>
+      <ul>
+        <li>Receipes</li>
+        <li>Comments</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
