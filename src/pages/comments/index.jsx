a81b5f24e7976263eff1831e7e3c2ce6c4a70951
AshKemp <kmuraliashwin@gmail.com>
2024-08-11 11:37:29 +0530

    get results from receipes, comments and render the data

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,7 +1,22 @@
+import useFetch from "../../hooks/use-fetch";
+
 function CommentsList() {
+  const { data, loading } = useFetch("https://dummyjson.com/comments");
+  if (loading) return <h1>Fetching comments!! Please wait.</h1>;
+
   return (
     <div>
       <h1>Comments List Page</h1>
+      <ul>
+        {data?.comments?.length > 0
+          ? data?.comments.map((comment) => (
+              <div>
+                <label>{comment?.body}</label>
+                <p>{comment?.likes}</p>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
 }
--- src/pages/receipes/index.jsx
+++ src/pages/receipes/index.jsx
@@ -8,10 +8,21 @@ function ReceipeList() {
   const resultFromCustomUseFetchHook = useFetch(
     "https://dummyjson.com/recipes"
   );
-  console.log(resultFromCustomUseFetchHook);
+  const { loading, data, error } = resultFromCustomUseFetchHook;
+  if (loading) return <h1>Fetching receipes!! Please wait</h1>;
   return (
     <div>
       <h1>Receipe List Page</h1>
+      <ul>
+        {data?.recipes?.length > 0
+          ? data?.recipes.map((recipeItem) => (
+              <div>
+                <img src={recipeItem?.image} />
+                <label>{recipeItem?.name}</label>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
 }
