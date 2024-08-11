7541c725e8c1d2c7979c04f724e6e13be4eff891
AshKemp <kmuraliashwin@gmail.com>
2024-08-11 11:11:38 +0530

    create custom hook to fetch API data

--- src/pages/receipes/index.jsx
+++ src/pages/receipes/index.jsx
@@ -1,8 +1,14 @@
 import { useLocation } from "react-router-dom";
+import useFetch from "../../hooks/use-fetch";
 
 function ReceipeList() {
   const location = useLocation();
   console.log(location);
+
+  const resultFromCustomUseFetchHook = useFetch(
+    "https://dummyjson.com/recipes"
+  );
+  console.log(resultFromCustomUseFetchHook);
   return (
     <div>
       <h1>Receipe List Page</h1>
